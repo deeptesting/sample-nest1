740ea57853d14c025b72afbfda7bb6d2657c5a44
Deepjyoty Roy <deepjyotyroy@gmail.com>
2023-03-10 11:59:59 +0530

    2nd commit

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -4,13 +4,17 @@ import { Controller, Get, Param } from '@nestjs/common';
 export class PostsController {
     constructor() {}
 
-    @Get()
-    findAll(): string {
-        return 'This action returns all posts';
+    @Get("")
+    findAll() {
+        return {
+            data: 'This action returns all posts'
+        };
     }
 
     @Get(":id")
-    findOne(@Param('id') id: number): string {
-        return 'This action returns one post of id ='+id;
+    findOne(@Param('id') id: number) {
+        return {
+            data: 'This action returns one post of id ='+id
+        }
     }
 }
